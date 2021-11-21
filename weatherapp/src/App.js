13150a43947d56fec2d5a6a0951d61a3d6932d0b
Akil Griffiths <akilgg@gmail.com>
2021-11-21 15:26:33 -0500

    Made Fetch to Api
    made a fetch tp the weather api

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+const [weather, setweather] = useState(
+  []
+)
+
+useEffect(() => {
+  fetch("http://api.openweathermap.org/data/2.5/forecast/daily?q=London,uk&cnt=5&APPID=b5421936a38d39b41a8d4eaca03180d2")
+  .then(resp => resp.json())
+  .then(data => setweather(data))
+})
+
+
+console.log(weather)
   return (
     <div className="App">
       <h1> Weather App</h1>
+      <h2>Your Weather For Today</h2>
+      <h2>Your 5 Day Forecast</h2>
     </div>
   );
 }
