1acbfa561fd555e25d916d602b1627b0a07da262
Gamoundo <akilgg@gmail.com>
2021-11-23 17:53:05 -0500

    set up 3 day forecast

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -7,20 +7,60 @@ function App() {
 const [weather, setweather] = useState(
   []
 )
-
-useEffect(() => {
-  fetch("http://api.openweathermap.org/data/2.5/forecast/daily?q=London,uk&cnt=5&APPID=b5421936a38d39b41a8d4eaca03180d2")
+useEffect(
+  () => {
+    fetch("http://api.weatherapi.com/v1/forecast.json?key=fe325598597046149c9172005212211&q=London&days=5")
   .then(resp => resp.json())
   .then(data => setweather(data))
-})
+  },
+  [weather],
+);
+  
+ 
+  
+ const displayWeather= (arr) => {
+      return(arr.map((ele) => {
+        return(
+          <div>
+            <h2>{ele.date}</h2>
+            <img src={ele.day.condition.icon} alt={ele.day.condition.text} />
+            <div className="temps">
+                <p>{ele.day.mintemp_f}</p>
+                <p>{ele.day.maxtemp_f}</p>
+            </div>
+
+          </div>
+        )
+      }))
+  }
+
+  const displayDaily= (obj) => {
+    return(
+      <div>
+        <h2>{obj.current.last_updated}</h2>
+        <img src={obj.current.condition.icon} alt={obj.current.condition.text} />
+        <div className="temps">
+          <p>{obj.current.feelslike_f}</p>
+          <p>{obj.current.temp_f}</p>
+        </div>
+      </div>
+    )
+  }
 
 
-console.log(weather)
+// console.log(weather)
   return (
     <div className="App">
       <h1> Weather App</h1>
       <h2>Your Weather For Today</h2>
-      <h2>Your 5 Day Forecast</h2>
+      <div>
+        {weather.current && displayDaily(weather)}
+      </div>
+      <h2>Your 3 Day Forecast</h2>
+      <div className="row">
+      {weather.forecast && displayWeather(weather.forecast.forecastday)}
+      </div>
+      
     </div>
   );
 }
